65db8e787cf3f6ddeb095cfba1895592ba550470
Aaron Florey <azza@jcaks.net>
2019-04-01 10:43:55 +1100

    add memory cache

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const prerender = require('prerender');
+const prerender = require("prerender");
 const server = prerender({
-  chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ],
+  chromeFlags: [
+    "--no-sandbox",
+    "--headless",
+    "--disable-gpu",
+    "--remote-debugging-port=9222",
+    "--hide-scrollbars"
+  ],
   forwardHeaders: true,
-  chromeLocation: '/usr/bin/chromium-browser'
+  chromeLocation: "/usr/bin/chromium-browser"
 });
 
-server.use(require('prerender-request-blacklist'));
+server.use(require("prerender-request-blacklist"));
+server.use(require("prerender-memory-cache"));
 server.use(prerender.blacklist());
 server.use(prerender.httpHeaders());
 
 server.start();
-
